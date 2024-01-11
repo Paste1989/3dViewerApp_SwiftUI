84b6c4ea764b008a6fdd49b5f7bfa2b8e5fed913
sbrezo <brezovac.sasa@gmail.com>
2024-01-11 11:14:50 +0100

    Initial Commit

---
+++ 3dViewerApp_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  3dViewerApp_SwiftUI
+//
+//  Created by Saša Brezovac on 11.01.2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3dViewerApp_SwiftUI/_dViewerApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _dViewerApp_SwiftUIApp.swift
+//  3dViewerApp_SwiftUI
+//
+//  Created by Saša Brezovac on 11.01.2024..
+//
+
+import SwiftUI
+
+@main
+struct _dViewerApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
